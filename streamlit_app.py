a08479865cb794d90f6ebefe9912ffbbfdbc18d5
jeffLTY <jefferson.job.application@gmail.com>
2025-10-26 16:10:57 +0800

    charting

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,42 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+import mysql.connector
+import pandas as pd
+import plotly.express as px
+
+# Read connection parameters securely from Streamlit secrets
+conn = mysql.connector.connect(
+    host=st.secrets["mysql"]["host"],
+    user=st.secrets["mysql"]["user"],
+    password=st.secrets["mysql"]["password"],
+    database=st.secrets["mysql"]["database"],
+    port=st.secrets["mysql"]["port"]
+)
+
+# Query data
+query = "SELECT * FROM gender_gap_job_sex_wide"
+gendergap_job_sex = pd.read_sql(query, conn)
+st.dataframe(gendergap_job_sex)
+
+# Close connection
+conn.close()
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+def _plot_series(series, series_name, series_index=0):
+  palette = list(sns.palettes.mpl_palette('Dark2'))
+  xs = series['year']
+  ys = series['gap_change_from_first']
+
+  plt.plot(xs, ys, label=series_name, color=palette[series_index % len(palette)])
+
+fig, ax = plt.subplots(figsize=(10, 5.2), layout='constrained')
+df_sorted = gendergap_edu_sex.sort_values('year', ascending=True)
+for i, (series_name, series) in enumerate(df_sorted.groupby('education')):
+  _plot_series(series, series_name, i)
+  fig.legend(title='education', bbox_to_anchor=(1, 1), loc='upper left')
+sns.despine(fig=fig, ax=ax)
+plt.xlabel('year')
+_ = plt.ylabel('gap_change_from_first')
